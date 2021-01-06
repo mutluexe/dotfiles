ab1935765f987a45a4c2bb68aa49838c808a89e0
mutluexe <mutlu.donmez@hotmail.com>
2021-01-06 10:19:52 +0300

    Add transparency

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,6 @@ set XDG_CACHE_HOME $HOME/.cache
 set XDG_DATA_HOME $HOME/.local/share
 
 alias tmux="tmux -2u -f "$XDG_CONFIG_HOME"/tmux/tmux.conf"
+set -gx PATH /home/mutluexe/.local/bin $PATH
+
+transset-df -a .80
