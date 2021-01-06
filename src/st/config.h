93ba9fdbca453418aa759a2e8df6b45822e1269a
mutluexe <mutlu.donmez@hotmail.com>
2021-01-06 12:59:28 +0300

    Fix tmux border problem solved by changing font

--- src/st/config.h
+++ src/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Droid Sans Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Menlo for Powerline:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 3;
 
 /*
